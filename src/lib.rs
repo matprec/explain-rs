154c48b10caa0872d657dfc7130df1045deb6ff2
MSleepyPanda <m.sleepypanda@gmail.com>
2017-10-31 23:45:34 +0100

    run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ impl ExtentOrNested {
     fn new(extent: Extented) -> ExtentOrNested {
         ExtentOrNested {
             extent,
-            subs: vec![]
+            subs: vec![],
         }
     }
 
@@ -69,13 +69,11 @@ impl ExtentExt for Vec<ExtentOrNested> {
         let result = self.iter_mut()
             .enumerate()
             .find(|&(_, ref it)| it.contains(extent))
-            .map(|(index, it)| {
-                if it.extent == extent {
-                    format!("_{}", index)
-                } else {
-                    let sub_id = it.push(extent).expect("Bug in sub elements!");
-                    format!("_{}{}", index, sub_id)
-                }
+            .map(|(index, it)| if it.extent == extent {
+                format!("_{}", index)
+            } else {
+                let sub_id = it.push(extent).expect("Bug in sub elements!");
+                format!("_{}{}", index, sub_id)
             });
 
         // TODO: Ugh fixme NLL
@@ -89,40 +87,35 @@ impl ExtentExt for Vec<ExtentOrNested> {
     }
 
     fn src(&self, buffer: &mut String, source: &str, init: usize) -> usize {
-        self
-            .iter()
-            .enumerate()
-            .fold(init, |last, (index, ext)| {
-                if last < ext.extent.0 {
-                    buffer.push_str(&source[last .. ext.extent.0].replace('<', "&lt").replace('>', "&gt"));
-                }
-                buffer.push_str(&format!("<span class='c{}'>", index));
-                let last = ext.subs.src(buffer, source, ext.extent.0);
-                let snippet = &source[cmp::max(ext.extent.0, last) .. ext.extent.1].replace('<', "&lt").replace('>', "&gt");
-                buffer.push_str(snippet);
-                buffer.push_str("</span>");
-                ext.extent.1
+        self.iter().enumerate().fold(init, |last, (index, ext)| {
+            if last < ext.extent.0 {
+                buffer.push_str(&source[last..ext.extent.0].replace('<', "&lt").replace(
+                    '>',
+                    "&gt",
+                ));
+            }
+            buffer.push_str(&format!("<span class='c{}'>", index));
+            let last = ext.subs.src(buffer, source, ext.extent.0);
+            let snippet = &source[cmp::max(ext.extent.0, last)..ext.extent.1]
+                .replace('<', "&lt")
+                .replace('>', "&gt");
+            buffer.push_str(snippet);
+            buffer.push_str("</span>");
+            ext.extent.1
         })
     }
 
 
     fn css(&self, stack: &mut Vec<usize>, selectors: &mut Vec<String>) {
-        self
-            .iter()
-            .enumerate()
-            .fold(stack, |mut stack, (index, ext)| {
+        self.iter().enumerate().fold(
+            stack,
+            |mut stack, (index, ext)| {
                 stack.push(index);
 
-                let ids: Vec<String> = stack
-                    .iter()
-                    .map(|i| i.to_string())
-                    .collect();
+                let ids: Vec<String> = stack.iter().map(|i| i.to_string()).collect();
                 let ids = ids.join("_");
 
-                let ids2: Vec<String> = stack
-                    .iter()
-                    .map(|i| i.to_string())
-                    .collect();
+                let ids2: Vec<String> = stack.iter().map(|i| i.to_string()).collect();
                 let ids2 = ids2.join(" > .c");
 
                 let id_string = format!("expl_{}", ids);
@@ -134,7 +127,8 @@ impl ExtentExt for Vec<ExtentOrNested> {
                 let value = stack.pop().expect("BUG: Index already got removed");
                 assert!(value == index);
                 stack
-            });
+            },
+        );
     }
 }
 
@@ -154,7 +148,12 @@ impl Context {
     pub fn push_src_ref(&mut self, extent: &Extent) {
         let id = self.ids.push_ext(extent.clone());
         let source = &self.source[extent.0..extent.1];
-        write!(self.buffer, "<span class='pre' id='expl{}' >{}</span>", id, source).unwrap()
+        write!(
+            self.buffer,
+            "<span class='pre' id='expl{}' >{}</span>",
+            id,
+            source
+        ).unwrap()
     }
 
     pub fn push_list_item(&mut self) {
@@ -184,7 +183,7 @@ impl Context {
 #[cfg(test)]
 mod test {
     use fuzzy_pickles;
-    use fuzzy_pickles::{Visit};
+    use fuzzy_pickles::Visit;
 
     use super::*;
     use std::collections::HashMap;
@@ -205,7 +204,7 @@ mod test {
                     is_list_item: false,
                 };
                 {
-                    let mut expl = html::Crate{ctx: &mut context};
+                    let mut expl = html::Crate { ctx: &mut context };
                     file.visit(&mut expl);
                 }
                 let curr_dir = std::env::current_dir().expect("Working dir should exist");
@@ -216,13 +215,19 @@ mod test {
                     .truncate(true)
                     .open("target/index.html")
                     .unwrap();
-                    writeln!(file, r#"<head>
+                writeln!(
+                    file,
+                    r#"<head>
 <base href="file:///{}">
 <link rel="stylesheet" href="explain-rs/res/highlightjs/styles/default.css">
 <script src="explain-rs/res/highlightjs/highlight.pack.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
-                    "#, curr_dir.display()).unwrap();
-                    writeln!(file, "
+                    "#,
+                    curr_dir.display()
+                ).unwrap();
+                writeln!(
+                    file,
+                    "
                 <style>
                 .pre {{
                     font-family: monospace;
@@ -233,12 +238,14 @@ mod test {
                 }}
                 {} {{\
                     background-color: yellow;\
-                }}</style></head>", context.css()).unwrap();
-                    writeln!(file, "{}", context.buffer).unwrap();
-                    writeln!(file, "<pre><code>{}</code></pre>", &context.src()).unwrap();
+                }}</style></head>",
+                    context.css()
+                ).unwrap();
+                writeln!(file, "{}", context.buffer).unwrap();
+                writeln!(file, "<pre><code>{}</code></pre>", &context.src()).unwrap();
 
             }
-            Result::Err(err) =>  {
+            Result::Err(err) => {
                 let detail = err.with_text(&src);
                 println!("{}", detail)
             }
@@ -249,8 +256,8 @@ mod test {
     fn test_extent_cache() {
         let mut extents: Vec<ExtentOrNested> = vec![];
         assert!(extents.push_ext((0, 5)) == "_0");
-        assert!(extents.push_ext((1,2)) == "_0_0");
-        assert!(extents.push_ext((3,4)) == "_0_1");
-        assert!(extents.push_ext((3,4)) == "_0_1");
+        assert!(extents.push_ext((1, 2)) == "_0_0");
+        assert!(extents.push_ext((3, 4)) == "_0_1");
+        assert!(extents.push_ext((3, 4)) == "_0_1");
     }
 }
